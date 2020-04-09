146282e6dc073e81bbcae911264ab0fcca2281a9
AlexanderAI <vlad.bovskiy@mail.ru>
2020-04-09 23:09:55 +0300

    Changes

--- CSharp/LR1/Cell.cs
+++ CSharp/LR1/Cell.cs
@@ -6,8 +6,7 @@ namespace Game2048
 {
     class Cell
     {
-        //public int Value { get; set; } = 0;
-        public int Value = 0;
+        public int Value { get; set; } = 0;
         public bool IsUnited { get; set; } = false;
     }
 }
