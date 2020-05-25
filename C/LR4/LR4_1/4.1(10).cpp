8e78893c6196cd29a8d77a7bd1a3be85c3f65fe6
AlexanderAI <vlad.bovskiy@mail.ru>
2020-05-25 16:22:35 +0300

    Add LR4

---
+++ C/LR4/LR4_1/4.1(10).cpp
@@ -0,0 +1,221 @@
+﻿#include <stdio.h> 
+#include <stdlib.h> 
+#include <string.h> 
+#include <windows.h>
+
+void MyToUpper(char* str)
+{
+	int i = 0;
+	while (str[i])
+	{
+		if ((str[i] >= -32 && str[i] <= -1) || (str[i] >= 97 && str[i] <= 122))
+			str[i] -= 32;
+		if (str[i] == -72)
+			str[i] -= 16;
+		i++;
+
+	}
+}
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	char str[81], ch;
+	int i = 0;
+	printf("Input string(length <= 80):\n");
+	while (((ch = getchar()) != '\n') && (i < 80)) {
+		str[i++] = ch;
+	}
+	str[i] = '\0';
+	printf("%d", 'ё');
+	printf("%d", 'Ё');
+	printf("%d", 'Ё' - 'ё');
+	MyToUpper(str);
+	i = 0;
+	while (str[i])
+	{
+		switch (str[i])
+		{
+		case 'А':
+		case 'A':
+			printf(".- ");
+			break;
+		case 'Б':
+		case 'B':
+			printf("-... ");
+			break;
+		case 'В':
+		case 'W':
+			printf(".-- ");
+			break;
+		case 'Г':
+		case 'G':
+			printf("--. ");
+			break;
+		case 'Д':
+		case 'D':
+			printf("-.. ");
+			break;
+		case 'Е':
+		case 'Ё':
+		case 'E':
+			printf(". ");
+			break;
+		case 'Ж':
+		case 'V':
+			printf("...- ");
+			break;
+		case 'З':
+		case 'Z':
+			printf("--.. ");
+			break;
+		case 'И':
+		case 'I':
+			printf(".. ");
+			break;
+		case 'Й':
+		case 'J':
+			printf(".--- ");
+			break;
+		case 'К':
+		case 'K':
+			printf("-.- ");
+			break;
+		case 'Л':
+		case 'L':
+			printf(".-.. ");
+			break;
+		case 'М':
+		case 'M':
+			printf("-- ");
+			break;
+		case 'Н':
+		case 'N':
+			printf("-. ");
+			break;
+		case 'О':
+		case 'O':
+			printf("--- ");
+			break;
+		case 'П':
+		case 'P':
+			printf(".--. ");
+			break;
+		case 'Р':
+		case 'R':
+			printf(".-. ");
+			break;
+		case 'С':
+		case 'S':
+			printf("... ");
+			break;
+		case 'Т':
+		case 'T':
+			printf("- ");
+			break;
+		case 'У':
+		case 'U':
+			printf("..- ");
+			break;
+		case 'Ф':
+		case 'F':
+			printf("..-. ");
+			break;
+		case 'Х':
+		case 'H':
+			printf(".... ");
+			break;
+		case 'Ц':
+		case 'C':
+			printf("-.-. ");
+			break;
+		case 'Ч':
+			printf("---. ");
+			break;
+		case 'Ш':
+			printf("---- ");
+			break;
+		case 'Щ':
+		case 'Q':
+			printf("--.- ");
+			break;
+		case 'Ъ':
+			printf(".--.-. ");
+			break;
+		case 'Ы':
+		case 'Y':
+			printf("-.-- ");
+			break;
+		case 'Ь':
+		case 'X':
+			printf("-..- ");
+			break;
+		case 'Э':
+			printf("...-...");
+			break;
+		case 'Ю':
+			printf("..-- ");
+			break;
+		case 'Я':
+			printf(".-.- ");
+			break;
+		case '1':
+			printf(".---- ");
+			break;
+		case '2':
+			printf("..--- ");
+			break;
+		case '3':
+			printf("...-- ");
+			break;
+		case '4':
+			printf("....- ");
+			break;
+		case '5':
+			printf("..... ");
+			break;
+		case '6':
+			printf("-.... ");
+			break;
+		case '7':
+			printf("--... ");
+			break;
+		case '8':
+			printf("---.. ");
+			break;
+		case '9':
+			printf("----. ");
+			break;
+		case '0':
+			printf("----- ");
+			break;
+		case '.':
+			printf("");
+			break;
+		case ',':
+			printf("");
+			break;
+		case ':':
+			printf("");
+			break;
+		case '?':
+			printf("");
+			break;
+		case '\'':
+			printf("");
+			break;
+		case '-':
+			printf("");
+			break;
+		case '/':
+			printf("");
+			break;
+		case ';':
+			printf("");
+			break;
+		default:
+			putchar('#');
+		}
+		i++;
+	}
+}
---
+++ C/LR4/LR4_2/4.2(10).cpp
@@ -0,0 +1,142 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int GetNumOfLinesInfile(FILE* f)
+{
+	int count = 0;
+	char buffer[255];
+	while (!feof(f))
+	{
+		fgets(buffer, 255, f);
+		count++;
+	}
+	fseek(f, 0, SEEK_SET);
+	return count;
+}
+
+void ToLowerStr(char* str)
+{
+	int i = 0;
+	while (str[i])
+		str[i++] = tolower(str[i]);
+}
+
+void ToUpperWordInStr(char* str, int start, int end)
+{
+	int i = start;
+	while (str[i] && i < end)
+		str[i++] = toupper(str[i]);
+}
+
+int IsSeparator(char c)
+{
+	char separators[] = " (){}&?!.,<>=#-+':^;%@*\"\n\t\\\0";
+	int i = 0;
+	while (separators[i])
+	{
+		if (separators[i] == c)
+			return 1;
+		i++;
+	}
+	return 0;
+}
+
+int IsKeyword(const char* word, char** KeywordsArray, int NumOfKeywords)
+{
+	for (int i = 0; i < NumOfKeywords; i++)
+		if (strcmp(word, KeywordsArray[i]) == 0)
+			return 1;
+	return 0;
+}
+
+void ConvertKeywordsInStr(char* str, char** KeywordsArray, int NumOfKeywords)
+{
+	int start = 0, end = 0;
+	char word[255];
+	for (int i = 0;str[i];i++)
+	{
+		if (!IsSeparator(str[i]) && (i == 0 || IsSeparator(str[i - 1])))
+			start = i;
+		if (!IsSeparator(str[i]) && (i == 0 || IsSeparator(str[i + 1])))
+		{
+			end = i;
+			strncpy_s(word, str + start, end - start + 1);
+			word[end - start + 1] = '\0';
+			if (IsKeyword(word, KeywordsArray, NumOfKeywords))
+				ToUpperWordInStr(str, start, end + 1);
+
+				//printf("%s\n", word);
+		}
+	}
+
+
+}
+int main()
+{
+	setlocale(LC_ALL, "Ru");
+	char fileName1[] = "D:\\Proga_2sem_re\\LR4\\LR4_2\\4.2(10)\\program.txt";
+	char fileName2[] = "D:\\Proga_2sem_re\\LR4\\LR4_2\\4.2(10)\\keywords.txt";
+	char fileName3[] = "D:\\Proga_2sem_re\\LR4\\LR4_2\\4.2(10)\\result.txt";
+	FILE* fpProgram;
+	fopen_s(&fpProgram, fileName1, "r");
+	if (fpProgram == NULL)
+	{
+		printf("Не удалось открыть файл");
+		return 1;
+	}
+	FILE* fpKeywords;
+	fopen_s(&fpKeywords, fileName2, "r");
+	if (fpKeywords == NULL)
+	{
+		printf("Не удалось открыть файл");
+		return 2;
+	}
+	FILE* fpResult;
+	fopen_s(&fpResult, fileName3, "w");
+	if (fpResult == NULL)
+	{
+		printf("Не удалось открыть файл");
+		return 3;
+	}
+
+	char buffer[255];
+	int NumOfKeywords = GetNumOfLinesInfile(fpKeywords);
+
+	char** KeywordsArray = (char**)malloc(NumOfKeywords * sizeof(char*));
+	for (int i = 0; i < NumOfKeywords; i++)
+	{
+		fgets(buffer, 255, fpKeywords);
+		if (buffer[strlen(buffer) - 1] == '\n')
+			buffer[strlen(buffer) - 1] = '\0';
+
+		KeywordsArray[i] = (char*)malloc((strlen(buffer) + 1) * sizeof(char));
+		strcpy_s(KeywordsArray[i], strlen(buffer) + 1, buffer);
+	}
+	fclose(fpKeywords);
+
+	while (!feof(fpProgram))
+	{
+		fgets(buffer, 255, fpProgram);
+		ToLowerStr(buffer);
+		ConvertKeywordsInStr(buffer, KeywordsArray, NumOfKeywords);
+		//fprintf(fpResult, buffer);
+		fputs(buffer, fpResult);
+		//printf("%s", buffer);
+	}
+
+
+
+
+
+
+
+
+
+
+	fclose(fpProgram);
+	fclose(fpResult);
+	return 0;
+}
