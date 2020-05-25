1c9439bddc3998c0ecefe36a5eec8f9fe58f544b
AlexanderAI <vlad.bovskiy@mail.ru>
2020-05-25 16:06:31 +0300

    Add LR2 again

---
+++ CSharp/LR2/7/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+
+namespace Task7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input your string:");
+            string input = Console.ReadLine();
+            string tmp;
+            foreach (char letter in input)
+            {
+                tmp = string.Format("{0:X} ", Convert.ToInt32(letter));
+                Console.Write(tmp.PadLeft(5, '0'));
+            }
+        }
+    }
+}
